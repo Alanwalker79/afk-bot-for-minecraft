8cc2ade683ccdc347199b3daa02f08355a0a042f
Alan <82333197+Alanwalker79@users.noreply.github.com>
2022-01-30 01:19:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ var connected = 0;
 var actions = [ 'forward', 'back', 'left', 'right']
 var lastaction;
 var pi = 3.14159;
+var port = data["port"];
 var moveinterval = 2; // 2 second movement interval
 var maxrandom = 5; // 0-5 seconds added to movement interval (randomly)
 var host = data["ip"];
@@ -16,6 +17,7 @@ var username = data["name"]
 var nightskip = data["auto-night-skip"]
 var bot = mineflayer.createBot({
   host: host,
+  port: port,
   username: username,
   version: "1.17.1"
 
